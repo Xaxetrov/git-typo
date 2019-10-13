1f9a45cf3734b7e4b3cd5fbcdd6a0bea45b9a70c
Xaxetrov <edern.haumont@insa-lyon.fr>
2019-10-13 16:38:33 +0200

    Code refactoring
    - Constants and ascii art are now separated from main functions
    - Random array element selection and display now has its own function

--- shellGitTypo.sh
+++ shellGitTypo.sh
@@ -1,17 +1,8 @@
-# Most ASCII art found on asciiart.eu, unknown artists
+# constants
 
-git() {
-  # enable zsh compatibility
-  if [ -n "$ZSH_VERSION" ]; then emulate -L ksh; fi
-  
-  # add
-  if [ "$1" = "dad" ]; then
-    curl -s https://icanhazdadjoke.com
-    echo
-    command git add "${@:2}"
+# ascii art - Most of them found on asciiart.eu, unknown artists
 
-  elif [ "$1" = "ass" ]; then
-    echo '
+gitTypoDonkey='
                           /\          /\
                          ( \\        // )
                           \ \\      // /
@@ -32,39 +23,36 @@ git() {
       | |  | |        | ||
       |_|  |_|        |_||
        \_\  \_\        \_\\'
-    command git add "${@:2}"
 
-  # blame
-  elif [ "$1" = "blam" ]; then
-    clear; echo '
+gitTypoExplosion0='
       )
      (
     .-`-.
     :   :
     :TNT:
     :___:'
-    sleep 0.5; clear; echo '
+gitTypoExplosion1='
     \|/
    - o -
     /-`-.
     :   :
     :TNT:
     :___:'
-    sleep 0.5; clear; echo '
+gitTypoExplosion2='
 
 
     .---.
     : | :
     :-o-:
     :_|_:'
-    sleep 0.5; clear; echo '
+gitTypoExplosion3='
 
 
     .---.
     (\|/)
     --0--
     (/|\)'
-    sleep 0.5; clear; echo "
+gitTypoExplosion4="
 
 
    '.\|/.'
@@ -72,14 +60,14 @@ git() {
    - -O- -
    (/   \)
    ,'/|\'."
-    sleep 0.5; clear; echo '
+gitTypoExplosion5='
 
 `.  \ | /  ,`
   `. `.` ,`
  ( .`.|.` .)
  - ~ -0- ~ -
  ('
-    sleep 0.5; clear; echo '
+gitTypoExplosion6='
 `,`|`.` )
   .` .`. `.
 ,`  / | \  `.
@@ -88,7 +76,7 @@ git() {
  . `` ,`. "
    ~ (   ~ -
 `'
-    sleep 0.5; clear; echo '
+gitTypoExplosion7='
 . `,`|` ` .
   .`  "  `
 ,   ` , `  `
@@ -98,6 +86,104 @@ git() {
     (  )
      ) /
     ,---.'
+    
+gitTypoPoop="
+  )  )
+ (   ) (
+  ) _   )
+   ( \_
+ _(_\ \)__
+(____\___))"
+
+# arrays
+
+# bible
+gitTypoBible[0]="And Jesus said unto Paul, 'Come forth, and receive everlasting life.' But Paul came in fifth, and received a toaster. - Order 25:3"
+gitTypoBible[1]="And the angel said unto him: 'Stop hitting yourself. Stop hitting yourself.' But low he could not. For the angel was hitting him with his own hands. - Irony 7:12"
+gitTypoBible[2]="Blessed are the cheesemakers.- Brian 22:1"
+gitTypoBible[3]="First shalt thou take out the Holy Pin. Then shalt thou count to three, no more, no less. Three shall be the number thou shalt count, and the number of the counting shall be three. Four shalt thou not count, neither count thou two, excepting that thou then proceed to three. Five is right out. Once the number three, being the third number, be reached, then lobbest thou thy Holy Hand Grenade of Antioch towards thy foe, who, being naughty in My sight, shall snuff it. - Arthur 1:2:5"
+gitTypoBible[4]="He's not the Messiah. He's a very naughty boy. - Brian 3:28"
+gitTypoBible[5]="Let those among you who are without sin be the first to throw the stone. The crowd looked at Jesus, believing he had a good point. Then Jesus picked up a stone. - Irony 8:19"
+gitTypoBible[6]="'This is my body'\n'This is my blood'\n'And this is your card'\nApostles go nuts. - Miracles 15:14"
+gitTypoBible[7]="Thou shalt have dominion over all the beasts except, of course, cats. - Felines 12:17"
+gitTypoBible[8]="Verily I say unto thee, lay thine eyes upon the field in which my fucks are grown. Behold that it is barren. - Crops 6:11"
+gitTypoBibleLength=9
+
+# german proverbs
+gitTypoGermanProverbs[0]="Alles hat ein Ende, nur die Wurst hat zwei."
+gitTypoGermanProverbs[1]="Da liegt der Hund begraben."
+gitTypoGermanProverbs[2]="Das ist mir Wurst."
+gitTypoGermanProverbs[3]="Das Blaue vom Himmel versprechen."
+gitTypoGermanProverbs[4]="Das kannst du deiner Oma erzählen."
+gitTypoGermanProverbs[5]="Das Leben ist kein Ponyhof."
+gitTypoGermanProverbs[6]="Die Kirche im Dorf lassen."
+gitTypoGermanProverbs[7]="Du gehst mir auf den Keks."
+gitTypoGermanProverbs[8]="Fremdschämen."
+gitTypoGermanProverbs[9]="Ich glaub ich spinne."
+gitTypoGermanProverbs[10]="Ich verstehe nur Bahnhof."
+gitTypoGermanProverbs[11]="Ins Gras beißen."
+gitTypoGermanProverbs[12]="Jetzt haben wir den Salat."
+gitTypoGermanProverbs[13]="Kabelsalat."
+gitTypoGermanProverbs[14]="Kein Schwein war da."
+gitTypoGermanProverbs[15]="Klappe zu, Affe tot."
+gitTypoGermanProverbs[16]="Kummerspeck."
+gitTypoGermanProverbs[17]="Leben ist kein Ponyhof."
+gitTypoGermanProverbs[18]="Leben wie Gott in Frankreich."
+gitTypoGermanProverbs[19]="Luftschloss."
+gitTypoGermanProverbs[20]="Mein Englisch ist unter aller Sau."
+gitTypoGermanProverbs[21]="Nul acht funfzehn."
+gitTypoGermanProverbs[22]="Schlafen wie ein Murmeltier."
+gitTypoGermanProverbs[23]="Sie hat einen Vogel."
+gitTypoGermanProverbs[24]="Sie hat nicht alle Tassen im Schrank."
+gitTypoGermanProverbs[25]="Sie spielt die beleidigte Leberwurst."
+gitTypoGermanProverbs[26]="Treppenwitz."
+gitTypoGermanProverbs[27]="Was Hänschen nicht lernt, lernt Hans nimmermehr."
+gitTypoGermanProverbs[28]="Wo sich Fuchs und Hase gute Nacht sagen."
+gitTypoGermanProverbsLength=29
+
+# sleep noises
+gitTypoSleepNoises[0]="*snore*"
+gitTypoSleepNoises[1]="zzz"
+gitTypoSleepNoisesLength=2
+
+# echo a random array element
+# pass array name as first parameter
+echoRandomArrayElement() {
+  argumentArrayName=$1
+  argumentArray=$argumentArrayName[@]
+  # get array length variable name
+  argumentArrayLength=${argumentArrayName}Length
+  # choose index array bounds
+  randomIndex=$((RANDOM % ${!argumentArrayLength}))
+  # print element at index
+  indirectValue=$argumentArrayName[$randomIndex]
+  echo ${!indirectValue}
+}
+
+git() {
+  # enable zsh compatibility
+  if [ -n "$ZSH_VERSION" ]; then emulate -L ksh; fi
+  
+  # add
+  if [ "$1" = "dad" ]; then
+    curl -s https://icanhazdadjoke.com
+    echo
+    command git add "${@:2}"
+
+  elif [ "$1" = "ass" ]; then
+    echo "$gitTypoDonkey"
+    command git add "${@:2}"
+
+  # blame
+  elif [ "$1" = "blam" ]; then
+    clear; echo "$gitTypoExplosion0"
+    sleep 0.5; clear; echo "$gitTypoExplosion1"
+    sleep 0.5; clear; echo "$gitTypoExplosion2"
+    sleep 0.5; clear; echo "$gitTypoExplosion3"
+    sleep 0.5; clear; echo "$gitTypoExplosion4"
+    sleep 0.5; clear; echo "$gitTypoExplosion5"
+    sleep 0.5; clear; echo "$gitTypoExplosion6"
+    sleep 0.5; clear; echo "$gitTypoExplosion7"
     sleep 1; clear
     command git blame "${@:2}"
 
@@ -107,20 +193,7 @@ git() {
     command git commit "${@:2}"
 
   elif [ "$1" = "commit" -a "$2" = "--amen" ]; then
-    # bible array generation
-    bible[0]="And Jesus said unto Paul, 'Come forth, and receive everlasting life.' But Paul came in fifth, and received a toaster. - Order 25:3"
-    bible[1]="And the angel said unto him: 'Stop hitting yourself. Stop hitting yourself.' But low he could not. For the angel was hitting him with his own hands. - Irony 7:12"
-    bible[2]="Blessed are the cheesemakers.- Brian 22:1"
-    bible[3]="First shalt thou take out the Holy Pin. Then shalt thou count to three, no more, no less. Three shall be the number thou shalt count, and the number of the counting shall be three. Four shalt thou not count, neither count thou two, excepting that thou then proceed to three. Five is right out. Once the number three, being the third number, be reached, then lobbest thou thy Holy Hand Grenade of Antioch towards thy foe, who, being naughty in My sight, shall snuff it. - Arthur 1:2:5"
-    bible[4]="He's not the Messiah. He's a very naughty boy. - Brian 3:28"
-    bible[5]="Let those among you who are without sin be the first to throw the stone. The crowd looked at Jesus, believing he had a good point. Then Jesus picked up a stone. - Irony 8:19"
-    bible[6]="'This is my body'\n'This is my blood'\n'And this is your card'\nApostles go nuts. - Miracles 15:14"
-    bible[7]="Thou shalt have dominion over all the beasts except, of course, cats. - Felines 12:17"
-    bible[8]="Verily I say unto thee, lay thine eyes upon the field in which my fucks are grown. Behold that it is barren. - Crops 6:11"
-    # citation choice and output
-    bibleLength=${#bible[@]}
-    randomIndex=$((RANDOM % $bibleLength))
-    echo ${bible[$randomIndex]}
+    echoRandomArrayElement gitTypoBible
     command git commit --amend "${@:3}"
 
   # remote
@@ -132,9 +205,6 @@ git() {
   # reset
   elif [ "$1" = "rest" ]; then
     echo "Thank you, I was tired..."
-    noise[0]="*snore*"
-    noise[1]="zzz"
-    noiseLength=${#noise[@]}
     if [ "$2" = "--hard" ]; then
       noiseDuration=10
     elif [ "$2" = "--soft" ]; then
@@ -144,9 +214,7 @@ git() {
     fi
     for ((i=0; i<$noiseDuration; i++)); do
       sleep 1
-      # noise choice and output
-      randomIndex=$((RANDOM % $noiseLength))
-      echo ${noise[$randomIndex]}
+      echoRandomArrayElement gitTypoSleepNoises
     done
     sleep 1
     echo "Wow nice nap !"
@@ -154,13 +222,7 @@ git() {
 
   # stash
   elif [ "$1" = "stash" -a "$2" = "poop" ]; then
-    echo "
-  )  )
- (   ) (
-  ) _   )
-   ( \_
- _(_\ \)__
-(____\___))"
+    echo "$gitTypoPoop"
     command git stash pop "${@:3}"
   else
     command git "$@"
@@ -191,39 +253,7 @@ if ! type gti &> /dev/null; then
 fi
 
 gut() {
-  # german proverb array generation
-  proverb[0]="Alles hat ein Ende, nur die Wurst hat zwei."
-  proverb[1]="Da liegt der Hund begraben."
-  proverb[2]="Das ist mir Wurst."
-  proverb[3]="Das Blaue vom Himmel versprechen."
-  proverb[4]="Das kannst du deiner Oma erzählen."
-  proverb[5]="Das Leben ist kein Ponyhof."
-  proverb[6]="Die Kirche im Dorf lassen."
-  proverb[7]="Du gehst mir auf den Keks."
-  proverb[8]="Fremdschämen."
-  proverb[9]="Ich glaub ich spinne."
-  proverb[10]="Ich verstehe nur Bahnhof."
-  proverb[11]="Ins Gras beißen."
-  proverb[12]="Jetzt haben wir den Salat."
-  proverb[13]="Kabelsalat."
-  proverb[14]="Kein Schwein war da."
-  proverb[15]="Klappe zu, Affe tot."
-  proverb[16]="Kummerspeck."
-  proverb[17]="Leben ist kein Ponyhof."
-  proverb[18]="Leben wie Gott in Frankreich."
-  proverb[19]="Luftschloss."
-  proverb[20]="Mein Englisch ist unter aller Sau."
-  proverb[21]="Nul acht funfzehn."
-  proverb[22]="Schlafen wie ein Murmeltier."
-  proverb[23]="Sie hat einen Vogel."
-  proverb[24]="Sie hat nicht alle Tassen im Schrank."
-  proverb[25]="Sie spielt die beleidigte Leberwurst."
-  proverb[26]="Treppenwitz."
-  proverb[27]="Was Hänschen nicht lernt, lernt Hans nimmermehr."
-  proverb[28]="Wo sich Fuchs und Hase gute Nacht sagen."
   # citation choice and output
-  proverbLength=${#proverb[@]}
-  randomIndex=$((RANDOM % $proverbLength))
-  echo ${proverb[$randomIndex]}
+  echoRandomArrayElement gitTypoGermanProverbs
   git "$@"
 }
